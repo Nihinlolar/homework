f181b70757c952093c1b0dc9d26c595105bd40d4
Innocent Akpareva <iamakostik@gmail.com>
2022-05-17 09:16:31 +0100

    Your First Homework

---
+++ main.py
@@ -0,0 +1,60 @@
+#First run this py file in vs code to see that it runs without any error, then start your homework.
+print("I can do all things")
+'''
+Instruction
+
+Find your questions in the comment and write the code below each comment
+
+After, code completion, 
+push your code to the same github repo you cloned the assignment.
+'''
+#################################################################################
+'''Question 1'''
+#declare a variable called "sports" \
+# whose value is a list that contains the element \
+# "football","tennis","volleyball","long jump"
+'''Your answer to Question 1 below'''
+#
+#
+'''Question 2'''
+#extend the sports list with the more_sports list below \
+# and save your new list in a variable called total_sports
+more_sports = ["basket ball", "wrestling", "boxing"]
+'''Your answer to Question 2 below'''
+#
+#
+'''Question 3'''
+#sort the elements in total_sports in descending order
+'''Your answer to Question 3'''
+#
+#
+'''Question 4'''
+#Delete the last element in total_sports and \
+# save the new list to a variable called clean_sports, print clean_sports
+'''Your answer to Question 4'''
+#
+#
+'''Question 5'''
+#create a list called counts whose value consist of integers 2,3,6,4,5,7,8,0
+#sort the counts list in decending order
+#calculate the mean of counts using sum() and len() functions, \
+# save the mean in a variable called mean and print the value
+'''Your answer to Question 5'''
+#
+#
+'''Question 6'''
+#create a dictionary called sports_dict with the elements in total_sports list as values\
+#and the index of each element as keys
+#print the key:value pair of sports_dict
+'''Your answer to question 6'''
+#
+#
+'''Question 7'''
+#Write the correct form of the code in the doc string below:
+#The output of the print statement: The index number of 'boxing' in more_sports list is 2
+'''
+print(
+    f'The index number of 'boxing' in more_sports list is {more_sports.index()}'
+)
+
+'''
